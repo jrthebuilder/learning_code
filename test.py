a257d0986ffa6dd3d5d9df9dc676f1c00b4302ea
Jerson Mezquita <jersonmezquita@Jersons-MBP.lan>
2015-07-04 09:44:09 -0400

    Basic String GA

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 __author__ = 'jersonmezquita'
 
-print "hello world! \n"*30
+def multi(user_input):
+    user_input = user_input*10
+    return user_input
+
+def multi_newline(user_input):
+    user_input = (user_input + "\n") * 10
+    return user_input
+
+
+x = raw_input("type your name: ")
+y = raw_input("which function u want to ex: multi or multi_newline: ")
+
+if y == "multi":
+    print multi(x)
+
+elif y == "multi_newline":
+    print multi_newline(x)
+
